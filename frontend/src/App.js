0e70c8dbfda38300903e13876770506398b8bb3c
dev0088 <ninjadev999@gmail.com>
2020-09-06 16:13:31 -0700

    Updated readme and root path.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -260,7 +260,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <BrowserRouter basename="/dashboard/">
+        <BrowserRouter basename="/">
           <CssBaseline />
           <ScrollToTop>
             <Routes />
