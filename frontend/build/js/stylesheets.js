5102c3c736b3a89fd3f02ba4f2a6d10c38c286eb
dev0088 <ninjadev999@gmail.com>
2020-09-12 03:23:38 -0700

    Added build directory and Updated docker-compose.

--- frontend/build/js/stylesheets.js
+++ frontend/build/js/stylesheets.js
@@ -1,14 +1,2 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["stylesheets"],{
-
-/***/ "./node_modules/react-gauge-chart/dist/GaugeChart/style.css":
-/*!******************************************************************!*\
-  !*** ./node_modules/react-gauge-chart/dist/GaugeChart/style.css ***!
-  \******************************************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./node_modules/react-gauge-chart/dist/GaugeChart/style.css?");
-
-/***/ })
-
-}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{1226:function(n,w,o){}}]);
+//# sourceMappingURL=stylesheets.js.map
